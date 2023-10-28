ab7cb4ac44431f898e14bd51633638ad5db8150d
Bünyamin Mercan <bunyaminmercan@Bunyamins-Air.home>
2023-10-28 19:15:41 +0300

    graphql init

---
+++ server/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+
+
+const schema = require('./schema/schema')
+/*
+var { buildSchema } = require("graphql")
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`)
+*/
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return "Hello world!"
+  },
+}
+const app = express()
+app.use('/graphql', graphqlHTTP({
+    schema,
+    //schema: schema,
+    //rootValue: root,
+    graphiql: true,
+}))
+app.listen(5005, () => {
+    console.log('server listeing on 5005')
+})
---
+++ server/schema/schema.js
@@ -0,0 +1,57 @@
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = require('graphql')
+const _ = require('lodash')
+
+
+const movies = [
+    {
+        id: "1",
+        title: 'title 1',
+        description:  'description 1',
+        year: 1993
+    },
+    {
+        id: "2",
+        title: 'title 2',
+        description:  'description 2',
+        year: 1993
+    },
+    {
+        id: "3",
+        title: 'title 3',
+        description:  'description 3',
+        year: 1993
+    },
+    {
+        id: "4",
+        title: 'title 4',
+        description:  'description 4',
+        year: 1993
+    }
+]
+const MovieType = new GraphQLObjectType({
+    name: 'Movie',
+    fields: () => ({
+        id: { type: GraphQLString },
+        title: { type: GraphQLString },
+        description: { type: GraphQLString },
+        year: { type: GraphQLInt },
+    })
+})
+
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQuery',
+    fields: {
+        movie: {
+            type: MovieType,
+            args: { id: { type: GraphQLString}},
+            resolve: (parent, args) => {
+                return _.find(movies,{ id: args.id})
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
